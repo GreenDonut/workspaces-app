93797347c04e90af08a46a22da4722af613f359e
Melody <melodybird2000@gmail.com>
2018-10-31 14:11:31 +1100

    img element

--- workspaces/src/components/LocationCard/LocationCard.js
+++ workspaces/src/components/LocationCard/LocationCard.js
@@ -5,10 +5,20 @@ class LocationCard extends Component {
     render() {
         return (
             <div>
-                <div className="br4 mw5 ma3 mw5">
-                    <h4 className="tc">Location Name</h4>
-                    <h4 className="tc">Address</h4>
-                </div>   
+                <div class="fl w-100 w-25-ns pa2">
+                    <div class="outline bg-white pv4 grow">
+                        <div className="center br4 mw5 ma3 mw5">
+                            <img className=""
+                                 src={require('../LocationList/LazyPanda.png')}
+                                 height="150" width="250"/>
+                            <h4 className="tc">Location Name</h4>
+                            <h4 className="tc">Address</h4>
+                        </div>   
+                        
+                         
+                    </div>
+                </div>
+              
             </div>
         );
     }
--- workspaces/src/components/LocationList/LocationList.js
+++ workspaces/src/components/LocationList/LocationList.js
@@ -8,74 +8,17 @@ class LocationList extends Component {
             <div>
                 <div class="mw9 center ph3-ns">
                 <div class="cf ph2-ns">
-                    <div class="fl w-100 w-25-ns pa2">
-                        <div class="outline bg-white pv4 grow">
-                      
-                          {  /*<img src={require('./LazyPanda.png')} height="200" width="250"/>*/}
-                          <LocationCard />
-                         
-                        </div>
-                    </div>
-                    <div class="fl w-100 w-25-ns pa2">
-                    <div class="outline bg-white pv4 grow">
-                             <LocationCard />
-                        </div>
-                    </div>
-                    <div class="fl w-100 w-25-ns pa2">
-                    <div class="outline bg-white pv4 grow">
-                             <LocationCard />
-                        </div>
-                    </div>
-                    <div class="fl w-100 w-25-ns pa2">
-                    <div class="outline bg-white pv4 grow">
-                             <LocationCard />
-                        </div>
-                    </div>
-                    <div class="fl w-100 w-25-ns pa2">
-                        <div class="outline bg-white pv4 grow">
-                             <LocationCard />
-                        </div>
-                    </div>
-                    <div class="fl w-100 w-25-ns pa2">
-                        <div class="outline bg-white pv4 grow">
-                             <LocationCard />
-                        </div>
-                    </div>
-                    <div class="fl w-100 w-25-ns pa2">
-                        <div class="outline bg-white pv4 grow">
-                             <LocationCard />
-                        </div>
-                    </div>
-                    <div class="fl w-100 w-25-ns pa2">
-                        <div class="outline bg-white pv4 grow">
-                             <LocationCard />
-                        </div>
-                    </div>
-                    <div class="fl w-100 w-25-ns pa2">
-                        <div class="outline bg-white pv4 grow">
-                             <LocationCard />
-                        </div>
-                    </div>
-                    <div class="fl w-100 w-25-ns pa2">
-                        <div class="outline bg-white pv4 grow">
-                             <LocationCard />
-                        </div>
-                    </div>
-                    <div class="fl w-100 w-25-ns pa2">
-                        <div class="outline bg-white pv4 grow">
-                             <LocationCard />
-                        </div>
-                    </div>
-                    <div class="fl w-100 w-25-ns pa2">
-                        <div class="outline bg-white pv4 grow">
-                             <LocationCard />
-                        </div>
-                    </div>
-                    <div class="fl w-100 w-25-ns pa2">
-                        <div class="outline bg-white pv4 grow">
-                             <LocationCard />
-                        </div>
-                    </div>
+                   <LocationCard />
+                   <LocationCard />
+                   <LocationCard />
+                   <LocationCard />
+                   <LocationCard />
+                   <LocationCard />
+                   <LocationCard />
+                   <LocationCard />
+                   <LocationCard />
+                   <LocationCard />
+                   <LocationCard />
                 </div>
                 </div>
             </div>
